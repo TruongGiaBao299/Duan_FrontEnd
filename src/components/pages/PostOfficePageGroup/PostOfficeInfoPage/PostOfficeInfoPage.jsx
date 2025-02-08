92871d1c24a627c8eed87a2adaaa03944ddd2b15
Truong Gia Bao <132983637+TruongGiaBao299@users.noreply.github.com>
2025-02-08 17:06:05 +0700

    fix

--- src/components/pages/PostOfficePageGroup/PostOfficeInfoPage/PostOfficeInfoPage.jsx
+++ src/components/pages/PostOfficePageGroup/PostOfficeInfoPage/PostOfficeInfoPage.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import PostOfficeNavbar from "../../../layout/PostOfficeNavbar/PostOfficeNavbar";
 import PostOfficeSidebar from "../../../layout/PostOfficeSidebar/PostOfficeSidebar";
-import Shipments from "../../../../containers/Shipments/Shipments";
 import styles from "./PostOfficeInfoPage.module.css";
-import ViewOrder from "../../../../containers/ViewOrder/ViewOrder";
-import PostOfficeGetOrder from "../../../../containers/PostOfficeGetOrder/PostOfficeGetOrder";
-import PostOfficeManageOrder from "../../../../containers/PostOfficeManageOrder/PostOfficeManageOrder";
-import PostOfficeHistory from "../../../../containers/PostOfficeHistory/PostOfficeHistory";
 import PostOfficeInfo from "../../../../containers/PostOfficeInfo/PostofficeInfo";
 
 const PostOfficeInfoPage = () => {
