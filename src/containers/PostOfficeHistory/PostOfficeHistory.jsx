cfa08848c616283ef8f80bd39901cb5936d2630a
Truong Gia Bao <132983637+TruongGiaBao299@users.noreply.github.com>
2025-03-07 21:05:54 +0700

    update 2

--- src/containers/PostOfficeHistory/PostOfficeHistory.jsx
+++ src/containers/PostOfficeHistory/PostOfficeHistory.jsx
@@ -80,105 +80,106 @@ const PostOfficeHistory = () => {
       {totalShippedOrders === 0 ? (
         <p>You don't have any shipped orders!</p>
       ) : (
-        <div>
-          {orders
-            .filter(
-              (order) =>
-                order.status === "shipped" && order.postOffice === postEmail
-            )
-            .map((order) => (
-              <>
-                <div className={styles.overcontent}>
-                  <div className={styles.topContent} key={order._id}>
-                    <div className={styles.OrderContent}>
-                      <div className={styles.addressInfo}>
-                        <div className={styles.statusGroup}>
-                          <p>
-                            <strong>Order ID:</strong> {order._id}
-                          </p>
-                          <p>
-                            <strong>Status:</strong> {order.status}
-                          </p>
-                        </div>
-                      </div>
-
-                      <div
-                        className={`${styles.contentgroup} ${
-                          selectedOrder?._id === order._id
-                            ? styles.selected
-                            : ""
-                        }`}
-                      >
-                        <div className={styles.addressGroup}>
-                          <p>
-                            <strong>From Address: </strong>
-                            {`${order.fromAddress}, ${order.fromDistrict},  ${order.fromWard}, ${order.fromCity}`}
-                          </p>
-
-                          {order.senderNumber}
-
-                          <p>
-                            <strong>To Address:</strong>{" "}
-                            {`${order.toAddress}, ${order.toDistrict},  ${order.toWard}, ${order.toCity}`}
-                          </p>
-
-                          {order.recipientNumber}
+        <div className={styles.overcontent}>
+          <div className={styles.ordercontent}>
+            {orders
+              .filter(
+                (order) =>
+                  order.status === "shipped" && order.postOffice === postEmail
+              )
+              .map((order) => (
+                <>
+                  <div>
+                    <div className={styles.topContent} key={order._id}>
+                      <div className={styles.OrderContent}>
+                        <div className={styles.addressInfo}>
+                          <div className={styles.statusGroup}>
+                            <p>
+                              <strong>Order ID:</strong> {order._id}
+                            </p>
+                            <p>
+                              <strong>Status:</strong> {order.status}
+                            </p>
+                          </div>
                         </div>
 
-                        <div className={styles.SentContent}>
-                          {/* Show Details Button */}
-                          <button onClick={() => handleShowDetails(order)}>
-                            Show Details
-                          </button>
+                        <div
+                          className={`${styles.contentgroup} ${
+                            selectedOrder?._id === order._id
+                              ? styles.selected
+                              : ""
+                          }`}
+                        >
+                          <div className={styles.addressGroup}>
+                            <p>
+                              <strong>From Address: </strong>
+                              {`${order.fromAddress}, ${order.fromDistrict},  ${order.fromWard}, ${order.fromCity}`}
+                            </p>
+
+                            {order.senderNumber}
+
+                            <p>
+                              <strong>To Address:</strong>{" "}
+                              {`${order.toAddress}, ${order.toDistrict},  ${order.toWard}, ${order.toCity}`}
+                            </p>
+
+                            {order.recipientNumber}
+                          </div>
+
+                          <div className={styles.SentContent}>
+                            {/* Show Details Button */}
+                            <button onClick={() => handleShowDetails(order)}>
+                              Show Details
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                </>
+              ))}
+          </div>
 
-                  <div className={styles.mapcontent}>
-                    <h3>Overview</h3>
-                    <div className={styles.map}>
-                      <MapContainer
-                        center={[10.7336, 106.6989]}
-                        zoom={13}
-                        style={{ height: "300px", width: "600px" }}
-                        zoomControl={false}
-                      >
-                        <TileLayer
-                          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        />
-                      </MapContainer>
-                    </div>
+          <div className={styles.mapcontent}>
+            <h3>Overview</h3>
+            <div className={styles.map}>
+              <MapContainer
+                center={[10.7336, 106.6989]}
+                zoom={13}
+                style={{ height: "300px", width: "600px" }}
+                zoomControl={false}
+              >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+              </MapContainer>
+            </div>
 
-                    {selectedOrder && (
-                      <div className={styles.orderDetails}>
-                        <p>
-                          <strong>Recipient Name:</strong>{" "}
-                          {selectedOrder.recipientName}
-                        </p>
-                        <p>
-                          <strong>Recipient Number:</strong>{" "}
-                          {selectedOrder.recipientNumber}
-                        </p>
-                        <p>
-                          <strong>To Address:</strong> {selectedOrder.toAddress}
-                          , {selectedOrder.toDistrict}, {selectedOrder.toCity}
-                        </p>
-                        <p>
-                          <strong>Price:</strong> {selectedOrder.price}
-                        </p>
+            {selectedOrder && (
+              <div className={styles.orderDetails}>
+                <p>
+                  <strong>Recipient Name:</strong> {selectedOrder.recipientName}
+                </p>
+                <p>
+                  <strong>Recipient Number:</strong>{" "}
+                  {selectedOrder.recipientNumber}
+                </p>
+                <p>
+                  <strong>To Address:</strong> {selectedOrder.toAddress},{" "}
+                  {selectedOrder.toDistrict}, {selectedOrder.toCity}
+                </p>
+                <p>
+                  <strong>Price:</strong> {selectedOrder.price}
+                </p>
 
-                        <p>
-                          <strong>Delivery Time:</strong>{" "}
-                          {selectedOrder.estimatedDeliveryTime}
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </>
-            ))}
+                <p>
+                  <strong>Delivery Time:</strong>{" "}
+                  {selectedOrder.estimatedDeliveryTime}
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       )}
     </div>
